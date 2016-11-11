c5090b4443ef67f65d880d003dac287c0ccf6618
Andrey Ovchinnikov <kukuruza-11@yandex.ru>
2016-11-11 17:18:27 +0300

    Empty!

--- app/js/common.js
+++ app/js/common.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-	var i = 0;
-	$('button').click(function(){
-		
-		// $(".if .words").fadeOut(100); 
-		$('.words').remove();
 
-		$(".if").append("<p class='words' style='transform: translateY(60px)'> "+ (i++) +" </p>");
-		$(".words").css("transform", "translateY(0px)");
-		// $(".if .words").fadeIn(1000); 
-
-
-	});
 
 });
